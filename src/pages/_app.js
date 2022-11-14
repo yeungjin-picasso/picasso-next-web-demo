91b47d0b849e10fc1977fa6779a6803249041567
woohyeon-dev <dngus3219@gmail.com>
2022-11-14 12:20:41 +0900

    Initial commit

---
+++ src/pages/_app.js
@@ -0,0 +1,21 @@
+import "@src/styles/globals.css";
+import GlobalStyle from "./GlobalStyle";
+
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
