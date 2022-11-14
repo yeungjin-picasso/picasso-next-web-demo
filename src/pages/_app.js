ba72aad7cb1a6dabe0981aa3f391db6275d719b1
woohyeon-dev <dngus3219@gmail.com>
2022-11-14 23:41:10 +0900

    chore: Add font, icon, image

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,17 @@
 import "@src/styles/globals.css";
 import GlobalStyle from "./GlobalStyle";
-
 import { ThemeProvider } from "styled-components";
 
 const theme = {
   colors: {
-    primary: "#0070f3",
+    lagunaBeach: "#E9D5C1",
+    sunsetOrange: "#FF4848",
+    dimGray: "#646464",
+    novel: "#9C9A9A",
+    darkLiver: "#4A4041",
+    cultured: "#F4F4F4",
+    tutu: "#FFE6E6",
+    veryLightGrey: "#CECDCD",
   },
 };
 
