26b92eaf251fe821bacd4caa5dd29069ccdb2fee
woohyeon-dev <dngus3219@gmail.com>
2022-11-22 00:27:12 +0900

    design: Add Shadow

--- src/components/atoms/ListElBox.js
+++ src/components/atoms/ListElBox.js
@@ -5,12 +5,15 @@ const Element = styled.div`
   overflow: hidden;
   width: calc(52vw - 4rem);
   margin-bottom: 1.4rem;
-  border-radius: 2.5rem;
+  border-radius: 2rem;
   padding: 0 2rem 1.7rem 2rem;
   background: ${({ theme }) => theme.colors.cultured};
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.09), 0 1px 2px rgba(0, 0, 0, 0.18);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 
   &:hover {
     background: ${({ theme }) => theme.colors.tutu};
+    box-shadow: 0 7px 14px rgba(0, 0, 0, 0.18), 0 5px 5px rgba(0, 0, 0, 0.16);
   }
 `;
 
