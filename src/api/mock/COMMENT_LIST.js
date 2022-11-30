5ac07020a54c59c2af74746cf33d671e470df2a9
woohyeon-dev <dngus3219@gmail.com>
2022-12-01 01:11:45 +0900

    chore: Modify the mock data

--- src/api/mock/COMMENT_LIST.js
+++ src/api/mock/COMMENT_LIST.js
@@ -1,65 +1,65 @@
 export const COMMENT_LIST = [
   {
-    id: 1,
+    comments_num: 1,
     writer: "wooowo",
     comment:
       "This post is about how I could use different features of Mockaroo which I felt would be very helpful for anyone testing data intensive applications.",
-    createdAt: "2021-12-04 10:57:17",
+    created_at: "2021-12-04 10:57:17",
   },
   {
-    id: 2,
+    comments_num: 2,
     writer: "Dilly",
     comment:
       "This post is about how I could use different features of Mockaroo which I felt would be very helpful for anyone testing data intensive applications.",
-    createdAt: "2021-12-26 07:46:15",
+    created_at: "2021-12-26 07:46:15",
   },
   {
-    id: 3,
+    comments_num: 3,
     writer: "Neel",
     comment:
       "This post is about how I could use different features of Mockaroo which I felt would be very helpful for anyone testing data intensive applications.",
-    createdAt: "2022-07-10 16:21:40",
+    created_at: "2022-07-10 16:21:40",
   },
   {
-    id: 4,
+    comments_num: 4,
     writer: "Merola",
     comment:
       "This post is about how I could use different features of Mockaroo which I felt would be very helpful for anyone testing data intensive applications.",
-    createdAt: "2021-12-24 02:36:02",
+    created_at: "2021-12-24 02:36:02",
   },
   {
-    id: 5,
+    comments_num: 5,
     writer: "Kakalina",
     comment:
       "This post is about how I could use different features of Mockaroo which I felt would be very helpful for anyone testing data intensive applications.",
-    createdAt: "2022-07-07 18:12:44",
+    created_at: "2022-07-07 18:12:44",
   },
   {
-    id: 6,
+    comments_num: 6,
     writer: "Ethe",
     comment:
       "This post is about how I could use different features of Mockaroo which I felt would be very helpful for anyone testing data intensive applications.",
-    createdAt: "2022-03-20 02:49:06",
+    created_at: "2022-03-20 02:49:06",
   },
   {
-    id: 7,
+    comments_num: 7,
     writer: "Iago",
     comment:
       "This post is about how I could use different features of Mockaroo which I felt would be very helpful for anyone testing data intensive applications.",
-    createdAt: "2022-09-04 06:31:53",
+    created_at: "2022-09-04 06:31:53",
   },
   {
-    id: 8,
+    comments_num: 8,
     writer: "Melba",
     comment:
       "This post is about how I could use different features of Mockaroo which I felt would be very helpful for anyone testing data intensive applications.",
-    createdAt: "2022-03-30 11:25:19",
+    created_at: "2022-03-30 11:25:19",
   },
   {
-    id: 9,
+    comments_num: 9,
     writer: "Travers",
     comment:
       "This post is about how I could use different features of Mockaroo which I felt would be very helpful for anyone testing data intensive applications.",
-    createdAt: "2021-12-28 15:23:07",
+    created_at: "2021-12-28 15:23:07",
   },
 ];
--- src/api/mock/QNA_LIST.js
+++ src/api/mock/QNA_LIST.js
@@ -1,229 +1,229 @@
 export const QNA_LIST = [
   {
-    id: 21,
+    question_num: 21,
     question: "What is the capital of Suriname?",
     writer: "wooowo",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry ageMaker",
     answer: "",
-    createdAt: "2022-11-21T10:15:30",
+    created_at: "2022-11-21T10:15:30",
     isPrivate: false,
   },
   {
-    id: 20,
+    question_num: 20,
     question: "What is the capital of Australia?",
     writer: "qwefqwf",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer: "",
-    createdAt: "2022-11-21T10:15:10",
+    created_at: "2022-11-21T10:15:10",
     isPrivate: false,
   },
   {
-    id: 19,
+    question_num: 19,
     question: "What is the capital of Argentina?",
     writer: "qwjfrtj",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy te",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standageMaker",
-    createdAt: "2022-11-21T10:14:40",
+    created_at: "2022-11-21T10:14:40",
     isPrivate: true,
   },
   {
-    id: 18,
+    question_num: 18,
     question: "What is the capital of Vietnam?",
     writer: "rtj546",
     description: "Lorem Ipsum s PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard duaker",
-    createdAt: "2022-11-21T10:14:30",
+    created_at: "2022-11-21T10:14:30",
     isPrivate: false,
   },
   {
-    id: 17,
+    question_num: 17,
     question: "What is the capital of Canada?",
     writer: "ytytm651",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-21T10:13:30",
+    created_at: "2022-11-21T10:13:30",
     isPrivate: true,
   },
   {
-    id: 16,
+    question_num: 16,
     question: "What is the capital of India?",
     writer: "wootrntrowo",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-21T10:12:30",
+    created_at: "2022-11-21T10:12:30",
     isPrivate: false,
   },
   {
-    id: 15,
+    question_num: 15,
     question: "What is the capital of Italy?",
     writer: "wooowo",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dumaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-21T10:11:30",
+    created_at: "2022-11-21T10:11:30",
     isPrivate: true,
   },
   {
-    id: 14,
+    question_num: 14,
     question: "What is the capital of Thailand?",
     writer: "wooowo",
     description:
       "L dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer: "Lorem Ipsum is sageMaker",
-    createdAt: "2022-11-20T10:15:30",
+    created_at: "2022-11-20T10:15:30",
     isPrivate: false,
   },
   {
-    id: 13,
+    question_num: 13,
     question: "What is the capital of Switzerland?",
     writer: "ergre",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-20T10:15:30",
+    created_at: "2022-11-20T10:15:30",
     isPrivate: true,
   },
   {
-    id: 12,
+    question_num: 12,
     question: "What is the capital of Ghana?",
     writer: "wooowo",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-18T10:15:30",
+    created_at: "2022-11-18T10:15:30",
     isPrivate: false,
   },
   {
-    id: 11,
+    question_num: 11,
     question: "What is the capital of Ukraine?",
     writer: "ergre",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-18T10:15:30",
+    created_at: "2022-11-18T10:15:30",
     isPrivate: false,
   },
   {
-    id: 10,
+    question_num: 10,
     question: "What is the capital of Chile?",
     writer: "qwdqwd",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-17T10:15:30",
+    created_at: "2022-11-17T10:15:30",
     isPrivate: false,
   },
   {
-    id: 9,
+    question_num: 9,
     question: "What is the capital of Korea?",
     writer: "fffgf",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-16T10:15:30",
+    created_at: "2022-11-16T10:15:30",
     isPrivate: false,
   },
   {
-    id: 8,
+    question_num: 8,
     question: "What is the capital of Japan?",
     writer: "ggwe54",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-16T10:15:30",
+    created_at: "2022-11-16T10:15:30",
     isPrivate: false,
   },
   {
-    id: 7,
+    question_num: 7,
     question: "What is the capital of France?",
     writer: "wooowo",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-15T10:15:30",
+    created_at: "2022-11-15T10:15:30",
     isPrivate: false,
   },
   {
-    id: 6,
+    question_num: 6,
     question: "What is the capital of Sweden?",
     writer: "kuyk545",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-10T10:15:30",
+    created_at: "2022-11-10T10:15:30",
     isPrivate: false,
   },
   {
-    id: 5,
+    question_num: 5,
     question: "What is the capital of Germany?",
     writer: "yuytj94",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-09T10:15:30",
+    created_at: "2022-11-09T10:15:30",
     isPrivate: true,
   },
   {
-    id: 4,
+    question_num: 4,
     question: "What is the capital of Brazil?",
     writer: "ytjtyj56",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-06T10:15:30",
+    created_at: "2022-11-06T10:15:30",
     isPrivate: false,
   },
   {
-    id: 3,
+    question_num: 3,
     question: "What is the capital of China?",
     writer: "wooowo",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-05T10:15:30",
+    created_at: "2022-11-05T10:15:30",
     isPrivate: true,
   },
   {
-    id: 2,
+    question_num: 2,
     question: "What is the capital of Russia?",
     writer: "trhtrh88",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-04T10:15:30",
+    created_at: "2022-11-04T10:15:30",
     isPrivate: false,
   },
   {
-    id: 1,
+    question_num: 1,
     question: "What is the capital of Denmark?",
     writer: "wooowo",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
     answer:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an and more recently with desktop publishing software like Aldus PageMaker",
-    createdAt: "2022-11-03T10:15:30",
+    created_at: "2022-11-03T10:15:30",
     isPrivate: true,
   },
 ];
